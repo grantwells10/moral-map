f79ca9c94c5b364f48417075d966ab799a4e6d88
Grant Wells <47901670+grantwells10@users.noreply.github.com>
2024-11-24 21:14:15 -0500

    path fix

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 // Routes
 const dilemmaRoutes = require('./routes/dilemmas');
